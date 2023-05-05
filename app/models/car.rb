2d9ea6cfbb8e390c1e10fbbf15dd58ee0de090b9
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-05 08:20:55 -0500

    Add validations.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,9 @@
 class Car < ApplicationRecord
+  validates :model, presence: true, length: { maximum: 250 }
+  validates :image, presence: true
+  validates :price, comparison: { greater_than_or_equal_to: 0 }
+  validates :accidents, comparison: { greater_than_or_equal_to: 0 }
+
   def as_json(options = {})
     super(options.merge(
       only: %i[id model image price brand year color accidents]
