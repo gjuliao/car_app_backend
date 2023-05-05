9d9958397d6cfc25c627110888ca6a39608df7b0
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-05 08:06:06 -0500

    Add as_json & dummy rented? method.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,15 @@
 class Car < ApplicationRecord
+  def as_json(options = {})
+    super(options.merge(
+      only: %i[id model image price brand year color accidents]
+    )).merge({ rented: rented?, electric: is_electric })
+  end
+
+  private
+
+  def rented?
+    # TODO: Implement this method
+    # Car.reservations.where(car_id: id).where("start_date <= ? AND end_date >= ?", Date.today, Date.today).any?
+    true
+  end
 end
