2f08eef6a20fc005382821fe3879fc45505cf0e0
belaymit <2belamit@gmail.com>
2023-05-05 21:20:38 +0300

    Configured api-endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # scope :api, defaults: { format: :json } do
-  #   scope :v1 do
-  #     devise_for :users, path: '', path_names: {
-  #       sign_in: 'login',
-  #       sign_out: 'logout',
-  #       registration: 'signup'
-  #     },
-  #     controllers: {
-  #       sessions: 'api/v1/users/sessions',
-  #       registrations: 'api/v1/users/registrations'
-  #     }
-  #   end
-  # end
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'api/v1/users/sessions',
+        registrations: 'api/v1/users/registrations'
+      }
+    end
+  end
   namespace :api do
     namespace :v1 do
       resources :cars
-      resources :users, only: %w[index] do
-        resources :reservations, only: %w[create index destroy]
+      resources :users do
+        resources :reservations
       end
     end
   end
-  # root 'api/v1/cars#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 first_user = User.create(name: 'Belay Birhanu', email: 'belay@gmail.com', password: '123456',
   password_confirmation: '123456')
-ufirst_ser = User.create(name: 'Giovanni Juliao', email: 'giovani@gmail.com', password: '123456',
+second_user = User.create(name: 'Giovanni Juliao', email: 'giovani@gmail.com', password: '123456',
   password_confirmation: '123456')
+
+third_user = User.create(name: 'Diego C', email: 'diego@gmail.com', password: '123456',
+    password_confirmation: '123456')
