b24639ca130ee3ce3d867541a14175d62b165ab9
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-07 12:24:21 -0500

    Add seed data for cars and random reservations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,64 +5,130 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'Belay Birhanu', email: 'belay@gmail.com', password: '123456',
+  password_confirmation: '123456')
+
+second_user = User.create(name: 'Giovanni Juliao', email: 'giovani@gmail.com', password: '123456',
+  password_confirmation: '123456')
+
+third_user = User.create(name: 'Diego C', email: 'diego@gmail.com', password: '123456',
+  password_confirmation: '123456')
+
+fourth_user = User.create(name: 'Paul S', email: 'paul@gmail.com', password: '123456',
+  password_confirmation: '123456')
 
 # Seed some Cars for testing purposes
-Car.create(model: 'Camry',
-  image: 'https://www.toyota.com/imgix/responsive/images/mlp/colorizer/2021/camry/1H1/1.png?q=85&fm=jpg&w=1024&fit=max&cs=strip&bg=fff',
-  description: 'If you\'re looking for a reliable, comfortable and spacious car, look no further than the Toyota \
-    Camry. The Camry is one of the best-selling sedans in the world, and for good reason. It offers a smooth ride, \
-    a spacious interior, and a range of features to suit your needs. Whether you need a car for commuting, family \
-    trips, or weekend adventures, the Camry has you covered. The Camry is also fuel-efficient, safe and durable, \
-    making it a smart choice for any driver. Don\'t miss this opportunity to own a Toyota Camry today!',
-  price: 25000,
-  brand: 'Toyota',
-  year: '2019-01-01',
-  color: 'White',
+first_car = Car.create(
+  model: '2018 Jaguar F-Type Coupe',
+  image: 'https://simaautoservice.se/wp-content/uploads/2021/07/car-image-transparent-background-8.png',
+  description: 'The 2018 Jaguar F-Type Coupe is a luxury sports car that boasts sleek and stylish design, \
+    exceptional performance, and advanced technology features.\n\nUnder the hood, it has a range of engine \
+    options, including a base 2.0-liter turbocharged four-cylinder engine that produces 296 horsepower \
+    and 295 lb-ft of torque, a 3.0-liter supercharged V6 engine that delivers 340 horsepower and 332 \
+    lb-ft of torque, and a top-of-the-line 5.0-liter supercharged V8 engine that generates 575 horsepower \
+    and 516 lb-ft of torque.',
+  price: 550,
+  brand: 'Land Rover',
+  year: '2018-01-01',
+  color: 'Gray',
   is_electric: false,
   accidents: 0
 )
 
-Car.create(model: 'Model S',
-  image: 'https://www.tesla.com/xNVh4yUEc3B9/04_Desktop.jpg',
-  description: 'Experience unparalleled performance with the 2022 Tesla Model S. With its all-electric powertrain \
-    and cutting-edge technology, the Model S can accelerate from 0 to 60 mph in just 1.98 seconds. The spacious \
-    interior is designed for comfort and convenience, with a 17-inch touchscreen display, premium sound system, \
-    and advanced driver assistance features. Plus, with a range of up to 405 miles on a single charge, the Model S \
-    is perfect for long road trips. Don\'t settle for less than the best - choose the 2022 Tesla Model S today!',
-  price: 79990,
-  brand: 'Tesla',
-  year: '2022-01-01',
-  color: 'Black',
+second_car = Car.create(
+  model: '2020 Jaguar F-Type Coupe',
+  image: 'https://carprices.ae/wp-content/uploads/2022/04/2021-Jaguar-F-Type-Coupe-Banner.png',
+  description: 'The 2020 Jaguar F-Type Coupe is a luxurious and sporty two-door coupe that exudes elegance and \
+  high performance.\n\nThe car is equipped with a range of powertrain options, including a base 2.0-liter \
+  turbocharged four-cylinder engine that produces 296 horsepower and 295 lb-ft of torque, a 3.0-liter supercharged \
+  V6 engine that delivers 340 horsepower and 332 lb-ft of torque, and a top-of-the-line 5.0-liter supercharged V8 \
+  engine that generates 575 horsepower and 516 lb-ft of torque. The engines are mated with an eight-speed automatic \
+  transmission, which can be controlled via paddle shifters mounted on the steering wheel.',
+  price: 650,
+  brand: 'Land Rover',
+  year: '2020-01-01',
+  color: 'Light gray',
   is_electric: true,
   accidents: 0
 )
 
-Car.create(model: 'Mustang',
-  image: 'https://www.ford.com/cmslibs/content/dam/vdm_ford/live/en_us/ford/nameplate/mustang/2021/collections/billboards/3_2/21_FRD_MST_1280x853_O.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg',
-  description: 'Get ready to feel the power of the 2021 Ford Mustang. With its classic styling, powerful engine, and \
-    advanced technology, the Mustang is a true American icon. Choose from a range of engines, including the 5.0L V8, \
-    and enjoy up to 480 horsepower and 420 lb.-ft. of torque. The Mustang also features advanced driver assistance \
-    features, including lane departure warning and adaptive cruise control, to keep you safe on the road. Don\'t \
-    settle for a boring car - get behind the wheel of the 2021 Ford Mustang today!',
-  price: 32500,
-  brand: 'Ford',
-  year: '2021-01-01',
+third_car = Car.create(
+  model: '2010 Audi TT RS Coupe',
+  image: 'https://www.freepnglogos.com/uploads/car-png/car-png-large-images-40.png',
+  description: 'The 2010 Audi TT RS Coupe is a high-performance sports car that delivers an exhilarating driving 
+    experience. It is powered by a 2.5-liter five-cylinder engine that produces an impressive 335 horsepower and 
+    332 lb-ft of torque. This engine is paired with a six-speed manual transmission and all-wheel drive, providing 
+    excellent handling and grip on the road.\n\nThe exterior of the TT RS Coupe is sleek and stylish, with a low 
+    profile and a distinctive front grille. Inside, the cabin is well-appointed with high-quality materials and 
+    advanced technology. Standard features include leather upholstery, automatic climate control, and a premium audio system.',
+  price: 450,
+  brand: 'Audi',
+  year: '2010-01-01',
   color: 'Red',
   is_electric: false,
   accidents: 1
 )
 
-Car.create(model: 'Civic',
-  image: 'https://automobiles.honda.com/-/media/Honda-Automobiles/Vehicles/2020/Civic-Sedan/2020_Honda_Civic_Sedan_Si_Hero_V3.jpg?h=675&w=1200&la=en&hash=FFB4D3C4A210B147E9B96A0A3E174CF94FA658E5',
-  description: 'Drive with confidence in the 2020 Honda Civic. This compact car offers a comfortable and spacious \
-    interior, a fuel-efficient engine, and advanced safety features to keep you and your passengers safe. Choose \
-    from a range of trims, including the sporty Si model, and enjoy features like Apple CarPlay, Android Auto, and \
-    Honda Sensing technology. With its sleek design and reliable performance, the Honda Civic is the perfect car \
-    for anyone who wants to enjoy the ride. Don\'t wait - test drive the 2020 Honda Civic today!',
-  price: 22500,
-  brand: 'Honda',
-  year: '2020-01-01',
-  color: 'Blue',
+fourth_car = Car.create(
+  model: '2014 VW Beetle Dune',
+  image: 'https://i.pinimg.com/originals/60/c6/05/60c6052cecf20a380007a5b1be98b68f.png',
+  description: 'The 2014 VW Beetle Dune is a special edition of the iconic VW Beetle that offers a unique and 
+    distinctive look. The Dune is inspired by the classic Baja Bugs, which were popular in off-road racing in 
+    the 1960s and 1970s. The Dune has a raised ride height, wider body, and unique front and rear bumpers, 
+    giving it a rugged and sporty appearance.',
+  price: 350,
+  brand: 'VolksWagen',
+  year: '2014-01-01',
+  color: 'Yellow',
   is_electric: false,
   accidents: 0
 )
+
+fifth_car = Car.create(
+  model: '2004 Hummer H2',
+  image: 'https://www.freepngimg.com/thumb/car/31469-4-car-transparent-image.png',
+  description: 'The 2014 VW Beetle Dune is a special edition of the iconic VW Beetle that offers a unique and 
+    distinctive look. The Dune is inspired by the classic Baja Bugs, which were popular in off-road racing in 
+    the 1960s and 1970s. The Dune has a raised ride height, wider body, and unique front and rear bumpers, 
+    giving it a rugged and sporty appearance.',
+  price: 1000,
+  brand: 'Hummer',
+  year: '2004-01-01',
+  color: 'Victory red',
+  is_electric: false,
+  accidents: 0
+)
+
+sixth_car = Car.create(
+  model: '2015 Audi Sport Quattro',
+  image: 'https://purepng.com/public/uploads/large/purepng.com-yellow-audi-caraudicars-961524670899johme.png',
+  description: 'he 2015 Audi Sport Quattro is a high-performance sports car that offers a thrilling driving 
+    experience. It is powered by a 4.0-liter turbocharged V8 engine that produces an impressive 552 horsepower 
+    and 516 lb-ft of torque. This engine is paired with an eight-speed automatic transmission and all-wheel drive, 
+    providing excellent handling and acceleration on the road.',
+  price: 700,
+  brand: 'Audi',
+  year: '2015-01-01',
+  color: 'Yellow',
+  is_electric: false,
+  accidents: 0
+)
+
+# Create some reservations for each car
+users = [first_user, second_user, third_user, fourth_user]
+cars = [first_car, second_car, third_car, fourth_car, fifth_car, sixth_car]
+
+cars.each do |car|
+  # Generate 3-5 random reservations for each car
+  start_date = Date.today
+  num_reservations = rand(3..5)
+  num_reservations.times do
+    # Generate random start and return dates
+    start_date += rand(1.day..5.days)
+    return_date = start_date + rand(1.day..5.days)
+
+    # Create a reservation for a random user and the current car
+    Reservation.create(start_date: start_date, return_date: return_date, user: users.sample, car: car)
+    start_date = return_date
+  end
+end
