a631a9e95568ed1c0f653090eeefed7a3b3dcc5b
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-07 12:15:39 -0500

    Refactor car model.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,5 @@
 class Car < ApplicationRecord
+  has_many :reservations, dependent: :destroy
   validates :model, presence: true, length: { maximum: 250 }
   validates :image, presence: true
   validates :price, comparison: { greater_than_or_equal_to: 0 }
@@ -6,15 +7,13 @@ class Car < ApplicationRecord
 
   def as_json(options = {})
     super(options.merge(
-      only: %i[id model image price brand year color accidents]
-    )).merge({ rented: rented?, electric: is_electric })
+      only: %i[id model image description price brand year color accidents]
+    )).merge({ rented: rented?, electric: is_electric, reservations: })
   end
 
   private
 
   def rented?
-    # TODO: Implement this method
-    # Car.reservations.where(car_id: id).where("start_date <= ? AND end_date >= ?", Date.today, Date.today).any?
-    true
+    reservations.where('start_date <= ? AND return_date >= ?', Date.today, Date.today).any?
   end
 end
