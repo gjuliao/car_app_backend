e8dc5b29bb88e5daf59e6f838323cc0dadb94efe
diego <dgcuenca@gmail.com>
2023-05-08 15:31:08 -0500

    Enable API fetch details of a specific reservation

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ReservationsController < ApplicationController
   before_action :find_reservation, only: %i[show update destroy]
-  # GET /reservations
+  # GET /reservations by user
   def index
     @reservations = Reservation.all
     if @reservations.empty?
@@ -10,15 +10,25 @@ class Api::V1::ReservationsController < ApplicationController
     end
   end
 
+  # GET /reservations/:id 
+  def show
+    if @reservation.nil?
+      render_response(:not_found)
+    else
+      render_response(:found, payload: @reservation)
+    end
+  end
+
   private
 
   def reservation_params
     params.require(:reservation).permit(:city, :start_date, :return_date)
   end
 
-  # def find_reservation
-  #   @reservation = Car.find(params[:id])
-  # rescue ActiveRecord::RecordNotFound
-  #   render_response(:not_found)
-  # end
+  def find_reservation
+    @reservation = Reservation.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render_response(:not_found)
+  end
+
 end
