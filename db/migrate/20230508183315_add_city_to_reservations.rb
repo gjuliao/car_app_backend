87bc094eafae6bbf952d31a71de80be0325a20a7
diego <dgcuenca@gmail.com>
2023-05-08 13:52:20 -0500

    Define validations for reservation model

--- db/migrate/20230508183315_add_city_to_reservations.rb
+++ db/migrate/20230508183315_add_city_to_reservations.rb
@@ -1,5 +1,5 @@
 class AddCityToReservations < ActiveRecord::Migration[7.0]
   def change
-    add_column :reservations, :city, :string
+    add_column :reservations, :city, :string, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_183315) do
     t.bigint "car_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "city"
+    t.string "city", null: false
     t.index ["car_id"], name: "index_reservations_on_car_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
