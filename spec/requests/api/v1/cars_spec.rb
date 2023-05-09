dfea7ec8dc8475ecca1fc2c229ae4bf9020d3732
diego <dgcuenca@gmail.com>
2023-05-09 14:51:35 -0500

    Add test to ensure all cars are retrieve

--- spec/requests/api/v1/cars_spec.rb
+++ spec/requests/api/v1/cars_spec.rb
@@ -5,23 +5,44 @@ RSpec.describe 'Api::V1::Cars', type: :request do
 
   before do
     @user = User.create!(name: 'Belay Birhanu', email: 'belay@gmail.com', password: '123456',
-    password_confirmation: '123456')
+      password_confirmation: '123456')
     login_as(@user, scope: :user)
-    @car = Car.create!(
+    @car1 = Car.create!(
       model: '2018 Jaguar F-Type Coupe',
-    image: 'https://simaautoservice.se/wp-content/uploads/2021/07/car-image-transparent-background-8.png',
-    description: 'The 2018 Jaguar F-Type Coupe is a luxury sports car',
-    price: 550,
-    brand: 'Land Rover',
-    year: '2018-01-01',
-    color: 'Gray',
-    is_electric: false,
-    accidents: 0
+      image: 'https://simaautoservice.se/wp-content/uploads/2021/07/car-image-transparent-background-8.png',
+      description: 'The 2018 Jaguar F-Type Coupe is a luxury sports car',
+      price: 550,
+      brand: 'Land Rover',
+      year: '2018-01-01',
+      color: 'Gray',
+      is_electric: false,
+      accidents: 0
     )
-    @reservation = Reservation.create!(start_date: '2023/05/09', return_date: '2023/05/12', user: @user, car: @car, city: 'Loja')
-  end
-
+    @car2 = Car.create!(
+      model: '2020 Jaguar F-Type Coupe',
+      image: 'https://simaautoservice.se/wp-content/uploads/2021/07/car-image-transparent-background-8.png',
+      description: 'The 2018 Jaguar F-Type Coupe is a luxury sports car',
+      price: 550,
+      brand: 'Land Rover',
+      year: '2020-01-01',
+      color: 'Red',
+      is_electric: true,
+      accidents: 2
+    )
+    @reservation = Reservation.create!(start_date: '2023/05/09', return_date: '2023/05/12', user: @user, car: @car1, city: 'Loja')
+    end
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      get api_v1_cars_path
+    end
+
+    it 'should returns a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'returns all cars' do
+      expect(assigns(:cars)).to match_array([@car1, @car2])
+    end
+
   end
 end
